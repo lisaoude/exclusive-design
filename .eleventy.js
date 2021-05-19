eeb376c42a40af9e0ee32e607ba56b66136dccf2
sanneduinkerx <sanne.duinker@hva.nl>
2021-05-19 14:09:48 +0200

    :art: styling toegepast

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('images')
   eleventyConfig.addPassthroughCopy('styles')
   return {
-    pssthroughFileCopy: true
+    passthroughFileCopy: true
   }
 }
 
