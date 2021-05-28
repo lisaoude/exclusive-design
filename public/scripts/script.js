8093361e2bae756fa96dab57a92e4e6a775e9aff
sanneduinkerx <sanne.duinker@hva.nl>
2021-05-28 14:51:22 +0200

    :confetti_ball: some styling done on text

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,7 @@
-console.log('hello')
-
 const button = document.querySelector('#klik');
 const nav = document.querySelector('nav');
 
 button.addEventListener('click', (e) => {
-    console.log('klik')
     nav.classList.toggle('show');
 })
 
