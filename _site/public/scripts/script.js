97988567d9c4e1ef577ab4f8de1966eecdbc1c4f
sanneduinkerx <sanne.duinker@hva.nl>
2021-06-16 22:41:21 +0200

    lagen klaar

--- _site/public/scripts/script.js
+++ _site/public/scripts/script.js
@@ -1,6 +1,34 @@
-const buttonShowMenu = document.querySelector('#klik');
 const menuDisabilities = document.querySelector('nav');
+const buttonShowMenu = document.querySelector('#klik');
 const hideMenu = document.querySelector('nav ul a');
+const selectBody = document.querySelector('body');
+
+const buttonMacula = document.querySelector('#macula');
+const maculaFilter = document.getElementById("maculaFilter");
+
+const buttonConcentration = document.querySelector('#concentratie');
+
+const buttonVasilis = document.querySelector('#vasilis');
+const selectForVasilis = document.querySelectorAll('main section');
+
+
+buttonMacula.addEventListener('click', () => {
+
+    maculaFilter.classList.remove('hideMacula');
+
+    selectBody.addEventListener('mousemove', (e) => {
+        var e = e || window.event;
+        var x = event.clientX;
+        var y = event.clientY;
+        maculaFilter.style.left = (e.clientX - 600) + 'px';
+        maculaFilter.style.top = (e.clientY - 600) + 'px';
+    })
+})
+
+
+buttonConcentration.addEventListener('click', () => {
+    selectBody.classList.toggle('concentratie');
+})
 
 buttonShowMenu.addEventListener('click', () => {
     menuDisabilities.classList.toggle('show');
@@ -11,4 +39,8 @@ hideMenu.addEventListener('click', (e) => {
     if(menuDisabilities.classList.contains('show')){
         menuDisabilities.classList.remove('show');
     }
+})
+
+buttonVasilis.addEventListener('click', (e) => {
+    selectForVasilis.forEach(element => element.classList.toggle('vasilisOn'))
 })
